5bdbee43e449e39b640a115621308ca667ea1aa4
dfuentes22 <dvf5104@gmail.com>
2018-02-16 23:12:05 -0500

    completed constructor

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,39 @@
 package io.zipcoder;
 
+        import java.util.ArrayList;
+        import java.util.Arrays;
+        import java.util.List;
+
 public class Student {
+
+    //variables
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    //Student Constructor
+    public Student(String firstName, String lastName, Double[] examScores) {
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.examScores = new ArrayList<Double>(Arrays.asList(examScores));
+        }
+    }
+
+    public void display(){
+        System.out.println(this.firstName);
+        System.out.println(this.lastName);
+        for(Double item : this.examScores){
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String []args) {
+        // Create Student object
+        Double[] scores = { 100.0, 95.0, 123.0, 96.0 };
+        Student test = new Student("Daniel", "Fuentes", scores);
+
+        test.display();
+    }
+
 }
